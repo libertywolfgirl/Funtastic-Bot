e13c4a2d318f7e9c256cee63f42420aede297fcd
Glitch (distinct-donut) <none>
2018-07-10 10:05:49 +0000

    🏨🏈 Checkpoint ./bot.js:1080301/3074

--- bot.js
+++ bot.js
@@ -63,10 +63,11 @@ app.post('/webhook', function (req, res) {
           console.log("Received message");
           //receivedMessage(event);
           const workspaceID = process.env.ASSISTANT_WORKSPACEID;
+          
                 var payload = {
                   workspace_id: workspaceID,
-                  context: req.body.context || {},
-                  input: req.body.input || {}
+                  context: req.body.context,
+                  input: event.message
                 };
                 assistant.message(payload, function (err, data) {
                   if (err) {
@@ -74,7 +75,8 @@ app.post('/webhook', function (req, res) {
                     console.log(err);
                     return res.status(err.code || 500).json(err);
                   }
-                  return res.json(updateMessage(payload, data));
+                  receivedMessage(event, updateMessage(payload, data));
+                  
               });
         } else if (event.postback) {
           console.log("Received postback");
@@ -90,6 +92,7 @@ app.post('/webhook', function (req, res) {
     // You must send back a 200, within 20 seconds, to let us know
     // you've successfully received the callback. Otherwise, the request
     // will time out and we will keep trying to resend.
+    res.sendStatus(200);
   }
 });
 
@@ -111,36 +114,8 @@ function receivedMessage(event, watsonResponse) {
   
 
   if (messageText) {
-    // If we receive a text message, check to see if it matches a keyword
-    // and send back the template example. Otherwise, just echo the text we received.
-    switch (messageText) {
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
-
-      default:
-        sendTextMessage(senderID, watsonResponse.output.text[0]);
-    }
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
+    sendTextMessage(senderID, watsonResponse.output.text[0]);
   }
-}
-
-function receivedPostback(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfPostback = event.timestamp;
-
-  // The 'payload' param is a developer-defined field which is set in a postback 
-  // button for Structured Messages. 
-  var payload = event.postback.payload;
-
-  console.log("Received postback for user %d and page %d with payload '%s' " + 
-    "at %d", senderID, recipientID, payload, timeOfPostback);
-
-  // When a postback is called, we'll send a message back to the sender to 
-  // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
 }
 
 //////////////////////////
@@ -155,53 +130,6 @@ function sendTextMessage(recipientId, messageText) {
       text: messageText
     }
   };
-
-  callSendAPI(messageData);
-}
-
-function sendGenericMessage(recipientId) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "generic",
-          elements: [{
-            title: "rift",
-            subtitle: "Next-generation virtual reality",
-            item_url: "https://www.oculus.com/en-us/rift/",               
-            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/rift/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for first bubble",
-            }],
-          }, {
-            title: "touch",
-            subtitle: "Your Hands, Now in VR",
-            item_url: "https://www.oculus.com/en-us/touch/",               
-            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/touch/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for second bubble",
-            }]
-          }]
-        }
-      }
-    }
-  };  
 
   callSendAPI(messageData);
 }
