98f7804797b7ffd9b1b36356ecb8cdc4e2bca17c
Glitch (facebookbotandwatsonassistant) <none>
2020-05-23 00:28:29 +0000

    📍🍭 Checkpoint ./bot.js:16231546/10221

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
 //
 // This is main file containing code implementing the Express server and functionality for the Express echo bot.
 //
-'use strict';
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
-const path = require('path');
+"use strict";
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const path = require("path");
 //const Watson = require('watson-developer-cloud'); // watson sdk
-const AssistantV2 = require('ibm-watson/assistant/v2');
-const { IamAuthenticator } = require('ibm-watson/auth');
+const AssistantV2 = require("ibm-watson/assistant/v2");
+const { IamAuthenticator } = require("ibm-watson/auth");
 
 /*const assistant = new Watson.ConversationV2({
             apiKey: process.env.ASSISTANT_APIKEY,
@@ -18,50 +18,54 @@ const { IamAuthenticator } = require('ibm-watson/auth');
         });*/
 
 const assistant = new AssistantV2({
-  version: '2020-04-01',
+  version: "2020-04-01",
   authenticator: new IamAuthenticator({
-    apikey: process.env.ASSISTANT_APIKEY,
+    apikey: process.env.ASSISTANT_APIKEY
   }),
-  url: process.env.ASSISTANT_URL,
+  url: process.env.ASSISTANT_URL
 });
 
-var messengerButton = "<html><head><title>Facebook Messenger Bot</title></head><body><h1>Facebook Messenger Bot</h1>This is a bot based on Messenger Platform QuickStart. For more details, see their <a href=\"https://developers.facebook.com/docs/messenger-platform/guides/quick-start\">docs</a>.<script src=\"https://button.glitch.me/button.js\" data-style=\"glitch\"></script><div class=\"glitchButton\" style=\"position:fixed;top:20px;right:20px;\"></div></body></html>";
+var messengerButton =
+  '<html><head><title>Facebook Messenger Bot</title></head><body><h1>Facebook Messenger Bot</h1>This is a bot based on Messenger Platform QuickStart. For more details, see their <a href="https://developers.facebook.com/docs/messenger-platform/guides/quick-start">docs</a>.<script src="https://button.glitch.me/button.js" data-style="glitch"></script><div class="glitchButton" style="position:fixed;top:20px;right:20px;"></div></body></html>';
 
 // The rest of the code implements the routes for our Express server.
 let app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 // Webhook validation
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
+app.get("/webhook", function(req, res) {
+  if (
+    req.query["hub.mode"] === "subscribe" &&
+    req.query["hub.verify_token"] === process.env.VERIFY_TOKEN
+  ) {
     console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
+    res.status(200).send(req.query["hub.challenge"]);
   } else {
     console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);          
+    res.sendStatus(403);
   }
 });
 
 // Display the web page
-app.get('/', function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
+app.get("/", function(req, res) {
+  res.writeHead(200, { "Content-Type": "text/html" });
   res.write(messengerButton);
   res.end();
 });
 
 // Message processing
-app.post('/webhook', function (req, res) {
+app.post("/webhook", function(req, res) {
   console.log(req.body);
   var data = req.body;
 
   // Make sure this is a page subscription
-  if (data.object === 'page') {
-    
+  if (data.object === "page") {
     // Iterate over each entry - there may be multiple if batched
     data.entry.forEach(function(entry) {
       var pageID = entry.id;
@@ -76,13 +80,13 @@ app.post('/webhook', function (req, res) {
             workspace_id: workspaceID,
             input: event.message
           };
-          assistant.message(payload, function (err, data) {
+          assistant.message(payload, function(err, data) {
             if (err) {
               console.log("error");
               console.log(err);
               return res.status(err.code || 500).json(err);
             }
-            receivedMessage(event, data);      
+            receivedMessage(event, data);
           });
         } else {
           console.log("Webhook received unknown event: ", event);
@@ -95,7 +99,7 @@ app.post('/webhook', function (req, res) {
     // You must send back a 200, within 20 seconds, to let us know
     // you've successfully received the callback. Otherwise, the request
     // will time out and we will keep trying to resend.
-    res.sendStatus(200);
+    //res.sendStatus(200);
   }
 });
 
@@ -106,8 +110,12 @@ function receivedMessage(event, watsonResponse) {
   var timeOfMessage = event.timestamp;
   var message = event.message;
 
-  console.log("Received message for user %d and page %d at %d with message:", 
-    senderID, recipientID, timeOfMessage);
+  console.log(
+    "Received message for user %d and page %d at %d with message:",
+    senderID,
+    recipientID,
+    timeOfMessage
+  );
   console.log(JSON.stringify(message));
 
   var messageId = message.mid;
@@ -135,28 +143,33 @@ function sendTextMessage(recipientId, messageText) {
 }
 
 function callSendAPI(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
-    method: 'POST',
-    json: messageData
+  request(
+    {
+      uri: "https://graph.facebook.com/v2.6/me/messages",
+      qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: messageData
+    },
+    function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var recipientId = body.recipient_id;
+        var messageId = body.message_id;
 
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-
-      console.log("Successfully sent generic message with id %s to recipient %s", 
-        messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      console.error(response);
-      console.error(error);
+        console.log(
+          "Successfully sent generic message with id %s to recipient %s",
+          messageId,
+          recipientId
+        );
+      } else {
+        console.error("Unable to send message.");
+        console.error(response);
+        console.error(error);
+      }
     }
-  });  
+  );
 }
 
 // Set Express to listen out for HTTP requests
-var server = app.listen(process.env.PORT || 3000, function () {
+var server = app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port %s", server.address().port);
 });
